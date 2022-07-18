edf76ec01e85801b949dbfdb23a80c5ac913022f
Milos Stojanovic <stojanovic98m@gmail.com>
2022-07-18 16:56:41 +0200

    inteface practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import "./App.css";
 
 function App() {
-  let juice: string | number;
-  juice = "orange juice";
+  // let juice: string | number;
+  // juice = "orange juice";
+
+  interface TestInterface {
+    id: number;
+    name: string;
+  }
+  const user: TestInterface = {
+    id: 0,
+    name: "Milos",
+  };
 
   return (
     <div>
-      <h4> {juice} </h4>
+      {/* <h4> {juice} </h4> */}
+      <h3> {user.name} </h3>
+      <p> {user.id} </p>
     </div>
   );
 }
